f1bca9c87b8494c17dd8e46ee9906beaeab75c5b
awaism47 <syed.munawar@strath.ac.uk>
2023-05-10 07:39:16 +0100

    updated addNewCard.js to update title with every new scene

--- addNewCard.js
+++ addNewCard.js
@@ -1,7 +1,11 @@
 document.getElementById("createScene").addEventListener("click",()=>{
     const original = document.getElementById('cards');
+    const numberOfCards=document.getElementsByClassName('card-title')
 
     const clone = original.cloneNode(true); 
-    clone.id="";
+    clone.id=numberOfCards.length;
     document.getElementById("scenes").appendChild(clone)
+    document.getElementById(clone.id).children[0].firstElementChild.textContent=`Building ${numberOfCards.length}`
+    console.log()
+    
 })
